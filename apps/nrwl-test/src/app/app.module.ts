7ee3bb8a4c947f431803c758ef97c22daa606ee6
Alla <alla@allas-mbp.lan>
2019-08-29 12:17:09 -0400

    added list and detail components + routing

--- apps/nrwl-test/src/app/app.module.ts
+++ apps/nrwl-test/src/app/app.module.ts
@@ -1,12 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListComponent } from './list/list.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [AppComponent, ListComponent, DetailsComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: ListComponent
+      },
+      {
+        path: ':id',
+        component: DetailsComponent
+      },
+      { path: '', component: ListComponent },
+      { path: '**', component: ListComponent }
+    ])
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ apps/nrwl-test/src/app/details/details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nrwl-test-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
